e4bd3bf78fe92468e070b4e081b5d32b5070046c
nfcl <2872786663@qq.com>
2022-10-21 23:19:39 +0800

    玩家信息json数据读取类

---
+++ Assets/Scripts/n_LocalPlayerData.cs
@@ -0,0 +1,24 @@
+﻿using Mirror;
+
+namespace n_LocalPlayerData
+{
+    public class LocalPlayerData
+    {
+        /// <summary>
+        /// 名称
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// 头像
+        /// </summary>
+        public int Avatar { get; set; }
+    }
+
+    public class Root
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public LocalPlayerData LocalPlayerData { get; set; }
+    }
+}
