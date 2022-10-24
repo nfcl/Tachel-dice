b19f071cbb8e497b83092f35ee64627d5680677e
nfcl <2872786663@qq.com>
2022-10-24 19:20:39 +0800

    增加了n_LocalPlayerData_LocalPlayerDataReaderWriter和n_LocalPlayerData_RootReaderWriter类用于n_LocalPlayerData.Root和n_LocalPlayerData.LocalPlayerData类的服务端传输

--- Assets/Scripts/n_LocalPlayerData.cs
+++ Assets/Scripts/n_LocalPlayerData.cs
@@ -1,4 +1,6 @@
-﻿namespace n_LocalPlayerData
+﻿using Mirror;
+
+namespace n_LocalPlayerData
 {
     public class LocalPlayerData
     {
@@ -18,5 +20,38 @@
         /// 
         /// </summary>
         public LocalPlayerData LocalPlayerData { get; set; }
+    }
+}
+
+/// <summary>
+/// n_LocalPlayerData.LocalPlayerData的序列化和反序列化类
+/// </summary>
+public static class n_LocalPlayerData_LocalPlayerDataReaderWriter
+{
+    public static void Write_n_LocalPlayerData_LocalPlayerData(this Mirror.NetworkWriter writer, n_LocalPlayerData.LocalPlayerData source)
+    {
+        writer.Write(source.Name);
+        writer.Write(source.Avatar);
+    }
+
+    public static n_LocalPlayerData.LocalPlayerData Read_n_LocalPlayerData_LocalPlayerData(this Mirror.NetworkReader reader)
+    {
+        return new n_LocalPlayerData.LocalPlayerData { Name = reader.Read<string>(), Avatar = reader.Read<int>() };
+    }
+}
+
+/// <summary>
+/// n_LocalPlayerData.Root的序列化和反序列化类
+/// </summary>
+public static class n_LocalPlayerData_RootReaderWriter
+{
+    public static void Write_n_LocalPlayerData_Root(this Mirror.NetworkWriter writer, n_LocalPlayerData.Root source)
+    {
+        writer.Write(source.LocalPlayerData);
+    }
+
+    public static n_LocalPlayerData.Root Read_n_LocalPlayerData_Root(this Mirror.NetworkReader reader)
+    {
+        return new n_LocalPlayerData.Root { LocalPlayerData = reader.Read<n_LocalPlayerData.LocalPlayerData>() };
     }
 }
