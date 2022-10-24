dc1cefaedf5d733c090118ce0de75e9904e7e135
nfcl <2872786663@qq.com>
2022-10-24 19:12:31 +0800

    1.增加private bool    _isPlayerExit;和public bool     IsPlayerExit    =>  _isPlayerExit;用于判断储存的玩家信息是否有效 2.增加了一个无参构造方法用于初始化 3.删除了一个带参构造方法public GameCanvasPlayerData(n_LocalPlayerData.Root source, bool isready = false) 4.增加了public void ClearPlayerInfo()成员方法用于清除玩家信息 5.增加了public void SetPlayerInfo(n_LocalPlayerData.Root data)成员方法用于设置新的玩家信息

--- Assets/Scripts/Game_Canvas/GameCanvasPlayerData.cs
+++ Assets/Scripts/Game_Canvas/GameCanvasPlayerData.cs
@@ -1,4 +1,6 @@
-﻿namespace Game_Canvas
+﻿using UnityEngine;
+
+namespace Game_Canvas
 {
     public class GameCanvasPlayerData
     {
@@ -14,23 +16,60 @@
         /// 玩家是否准备
         /// </summary>
         private bool    _isReady;
+        /// <summary>
+        /// 当前存储的玩家信息有效
+        /// </summary>
+        private bool    _isPlayerExit;
+        
+        public string   Name            =>  _name;
+        public int      Avatar          =>  _avatar;
+        public bool     IsReady         =>  _isReady;
+        public bool     IsPlayerExit    =>  _isPlayerExit;
 
-        public string   Name        =>  _name;
-        public int      Avatar      =>  _avatar;
-        public bool     IsReady     =>  _isReady;
-
-        public GameCanvasPlayerData(n_LocalPlayerData.Root source, bool isready = false)
+        public GameCanvasPlayerData()
         {
-            _name       = source.LocalPlayerData.Name;
-            _avatar     = source.LocalPlayerData.Avatar;
-            _isReady    = isready;
+            _name           = "null";
+            _avatar         = 1;
+            _isReady        = false;
+            _isPlayerExit   = false;
         }
 
-        public GameCanvasPlayerData(string name, int avatar, bool isready = false)
+        /// <summary>
+        /// 服务端与客户端传输数据用构造
+        /// </summary>
+        /// <param name="name">玩家姓名</param>
+        /// <param name="avatar">玩家头像</param>
+        /// <param name="isready">是否已准备</param>
+        /// <param name="isPlayerExit">玩家是否有效</param>
+        public GameCanvasPlayerData(string name, int avatar, bool isready = false, bool isPlayerExit = false)
         {
-            _name       = name;
-            _avatar     = avatar;
-            _isReady    = isready;
+            _name           = name;
+            _avatar         = avatar;
+            _isReady        = isready;
+            _isPlayerExit   = isPlayerExit;
+        }
+
+        /// <summary>
+        /// 清除玩家信息
+        /// </summary>
+        public void ClearPlayerInfo()
+        {
+            _name           = "null";
+            _avatar         = 1;
+            _isReady        = false;
+            _isPlayerExit   = false;
+        }
+
+        /// <summary>
+        /// 设置玩家信息
+        /// </summary>
+        /// <param name="data">玩家信息</param>
+        public void SetPlayerInfo(n_LocalPlayerData.Root data)
+        {
+            _name           = data.LocalPlayerData.Name;
+            _avatar         = data.LocalPlayerData.Avatar;
+            _isReady        = false;
+            _isPlayerExit   = true;
         }
 
         /// <summary>
@@ -65,17 +104,19 @@ public static class GameCanvasPlayerDataReaderWriter
             writer.Write("null");
             writer.Write(1);
             writer.Write(false);
+            writer.Write(false);
         }
         else
         {
             writer.Write(source.Name);
             writer.Write(source.Avatar);
             writer.Write(source.IsReady);
+            writer.Write(source.IsPlayerExit);
         }
     }
 
     public static Game_Canvas.GameCanvasPlayerData Read_GameCanvasPlayerData(this Mirror.NetworkReader reader)
     {
-        return new Game_Canvas.GameCanvasPlayerData(reader.Read<string>(), reader.Read<int>(),reader.Read<bool>());
+        return new Game_Canvas.GameCanvasPlayerData(reader.Read<string>(), reader.Read<int>(), reader.Read<bool>(), reader.Read<bool>());
     }
 }
