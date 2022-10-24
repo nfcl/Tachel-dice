dc04c38e674db181be762ecb83bb9c775e9deefc
nfcl <2872786663@qq.com>
2022-10-24 19:41:39 +0800

    增加了用于服务端数据传输的类

--- Assets/Scripts/Game_Canvas/GameCanvasPlayerData.cs
+++ Assets/Scripts/Game_Canvas/GameCanvasPlayerData.cs
@@ -89,3 +89,22 @@
         }
     }
 }
+
+/// <summary>
+/// GameCanvasPlayerData的序列化和反序列化类
+/// </summary>
+public static class GameCanvasPlayerDataReaderWriter
+{
+    public static void Write_GameCanvasPlayerData(this Mirror.NetworkWriter writer, Game_Canvas.GameCanvasPlayerData source)
+    {
+        writer.Write(source.Name);
+        writer.Write(source.Avatar);
+        writer.Write(source.IsReady);
+        writer.Write(source.IsPlayerExit);
+    }
+
+    public static Game_Canvas.GameCanvasPlayerData Read_GameCanvasPlayerData(this Mirror.NetworkReader reader)
+    {
+        return new Game_Canvas.GameCanvasPlayerData(reader.Read<string>(), reader.Read<int>(), reader.Read<bool>(), reader.Read<bool>());
+    }
+}
